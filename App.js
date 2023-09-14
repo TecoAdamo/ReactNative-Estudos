3e95e914ae7fff44ea7d14326996bd758bbbf929
Mateus Adamo <adamomateus478@gmail.com>
2023-09-14 03:51:08 -0300

    Tab Navigation

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from './src/Screens/Home/Home';
+import Contact from './src/Screens/Contact/Contact';
+import Menu from './src/Screens/Menu/Menu';
+import Perfil from './src/Screens/Perfil/Perfil';
+
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => {
+              return <Ionicons name='home-outline' color={color} size={size}/>
+            }
+          }}
+
+        />
+        <Tab.Screen 
+        name="Contato" 
+        component={Contact} 
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name='call-outline' color={color} size={size}/>
+          }
+        }}
+        />
+        <Tab.Screen 
+        name="Perfil" 
+        component={Perfil} 
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name='person-circle-outline' color={color} size={size}/>
+          }
+        }}
+        />
+        <Tab.Screen 
+        name="Menu" 
+        component={Menu} 
+        options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name='menu-outline' color={color} size={size}/>
+          }
+        }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
